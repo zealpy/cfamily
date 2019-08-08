155ea93066694f082ba5cfcf8befc081d3595a17
syedmahmoodroomi <syedmahmood.neuron@gmail.com>
2019-08-09 03:27:20 +0530

    This is my first commit!

---
+++ categoryApp/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, get_object_or_404
+from categoryApp.models import Category, Post
+
+
+def index(request):
+    return render(request, 'home.html')
+
+def home(request):
+    return render(request, 'home.html')
+
+def show_category(request,hierarchy= None):
+    category_slug = hierarchy.split('/')
+    category_queryset = list(Category.objects.all())
+    all_slugs = [ x.slug for x in category_queryset ]
+    parent = None
+    for slug in category_slug:
+        if slug in all_slugs:
+            parent = get_object_or_404(Category,slug=slug,parent=parent)
+        else:
+            instance = get_object_or_404(Post, slug=slug)
+            breadcrumbs_link = instance.get_cat_list()
+            category_name = [' '.join(i.split('/')[-1].split('-')) for i in breadcrumbs_link]
+            breadcrumbs = zip(breadcrumbs_link, category_name)
+            return render(request, "postDetail.html", {'instance':instance,'breadcrumbs':breadcrumbs})
+
+    return render(request,"categories.html",{'post_set':parent.post_set.all(),'sub_categories':parent.children.all()})
+
