978af14ef293fce8dcc35233671c18f5fa8efdef
syedmahmoodroomi <syedmahmood.neuron@gmail.com>
2019-08-24 10:47:24 +0530

    my third commit to roomi

--- Cfamily/urls.py
+++ Cfamily/urls.py
@@ -1,4 +1,4 @@
-"""CFamily URL Configuration
+"""Cfamily URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('CategoryApp.urls'))
+    path('', include('Category.urls'))
 ]
--- Cfamily/wsgi.py
+++ Cfamily/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for CFamily project.
+WSGI config for Cfamily project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CFamily.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Cfamily.settings')
 
 application = get_wsgi_application()
